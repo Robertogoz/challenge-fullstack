3318964283ac7ca76effeaf91589eca8baf11e42
robertogoz <gozjunior@hotmail.com>
2022-07-28 13:59:31 -0300

    feat: configure CORS

--- backend/src/main/java/com/leadfinder/challenge/controllers/RobotController.java
+++ backend/src/main/java/com/leadfinder/challenge/controllers/RobotController.java
@@ -3,13 +3,11 @@ package com.leadfinder.challenge.controllers;
 import com.leadfinder.challenge.services.IRobotService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rest")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class RobotController {
 
     final IRobotService _robotService;
@@ -23,14 +21,14 @@ public class RobotController {
         this._robotService.setX(0);
         this._robotService.setY(0);
         this._robotService.setDirection('N');
-        if(this._robotService.validateCommands(command) == false) {
+        if(!this._robotService.validateCommands(command)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid command.");
         }
         try{
         String coordinates = this._robotService.handleCoords(command);
         return ResponseEntity.status(HttpStatus.OK).body(coordinates);
         } catch(Error err) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid moviments.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid position.");
         }
     }
 }
